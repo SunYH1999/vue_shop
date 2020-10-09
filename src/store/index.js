38c97d75d33db368bc9422c6de6772e5c1532ea1
孙宇航 <sunyuhangvika@163.com>
2020-10-09 20:16:30 +0800

    1009

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+// aa
+export default new Vuex.Store({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
